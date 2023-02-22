dce7594a9b3ef89920c1a46b0200ed6e6066037b
Syifaul Qolbi <syifaul@pop-os.localdomain>
2023-02-22 13:13:31 +0700

    portfolio update

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#10b981",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
